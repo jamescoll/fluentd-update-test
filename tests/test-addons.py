f5a50c8f43966408b73f3e60a287fdb86f5e2797
James Coll <james.evin.coll@gmail.com>
2022-07-31 11:08:39 +0100

    Update test-addons.py

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -5,9 +5,14 @@ from utils import microk8s_enable, wait_for_pod_state, microk8s_disable
 
 
 class TestAddons(object):
-    def test_python_demo_nginx(self):
-        microk8s_enable("python-hello-k8s")
-        wait_for_pod_state("", "default", "running", label="app=python-demo-nginx")
-        status = yaml.safe_load(sh.microk8s.status(format="yaml").stdout)
-        expected = {"python-hello-k8s": "enabled"}
-        microk8s_disable("python-hello-k8s")
+    def validate_fluentd():
+    """
+    Validate fluentd
+    """
+    if platform.machine() != "x86_64":
+        print("Fluentd tests are only relevant in x86 architectures")
+        return
+
+    wait_for_pod_state("elasticsearch-logging-0", "kube-system", "running")
+    wait_for_pod_state("", "kube-system", "running", label="k8s-app=fluentd-es")
+    wait_for_pod_state("", "kube-system", "running", label="k8s-app=kibana-logging")
